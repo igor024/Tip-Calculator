763b9f122f047cd6dc6fb7ca37bb85f0957d59ff
igor023 <thedarthdot@gmail.com>
2024-03-07 23:52:11 -0500

    more stuff

--- Tip Calculator/ContentView.swift
+++ Tip Calculator/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var amount = 0
-    @State private var percent: Double? = 0
+    @State private var percent: Double = 20
+    @State private var tax: Int = 8
+    @State private var includeTax: Bool = false
+
 
     var body: some View {
         VStack {
@@ -25,7 +28,17 @@ struct ContentView: View {
                     .font(.body)
                     .padding(0)
             }
-            
+            Text("Tip: \(Int(round(percent)))%")
+                .font(.body)
+                .padding(0)
+            Slider(value: $percent, in: 10...30)
+            Toggle("Include Tax", isOn: $includeTax)
+            if(includeTax) {
+                Stepper("Tax rate: \(tax)%", value: $tax, in: 1...15)
+            }
+            Button("Calculate") {
+                
+            }
         }
         .padding()
     }
